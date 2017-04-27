b627116825b906afa887ec60564e09c5bb6d8dcc
Outtay <otti.florian@gmail.com>
2017-04-27 15:58:04 +0200

    samples and first A of first week

---
+++ week1/A.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+
+//Source for everything: your slides
+int main (){
+    
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) {
+        
+        std::string s1;
+        char s2[101];
+        scanf ("%100s" , s2 );
+        printf ( "Case #%d: %s" , i , s2 );
+
+    }
+    return 0;
+}
