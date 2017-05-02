056c1beedd98f4547d5047fdb7b243beda1a2e89
Outtay <otti.florian@gmail.com>
2017-05-03 00:09:43 +0200

    new shit

--- week1/A.cpp
+++ week1/A.cpp
@@ -8,9 +8,8 @@ int main (){
     scanf("%d", &t);
     for (int i = 1; i <= t; i++) {
         
-        std::string s1;
-        char s2[101];
-        scanf ("%100s" , s2 );
+        char s1[101];
+        scanf ("%100s" , s1 );
         printf ( "Case #%d: Hello %s!" , i , s2 );
 
     }
