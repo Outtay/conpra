05c9f00e195cb70cac2355f54c3e8f0ab0abfc12
Florian Otti <otti.florian@gmail.com>
2017-06-04 16:42:26 +0200

    uploading for debugging

--- week5/A.cpp
+++ week5/A.cpp
@@ -4,14 +4,243 @@
 #include <algorithm>
 #include <string>
 
+struct Edge {
+    int to;
+    int capacity;
+    int flow;
+    int start; //not necessary since we're in an adj List and the index of the vector element is the start
+};
+
+
+class Graph {
+    public: 
+        int vertexCount;
+        std::vector <int> height; 
+        std::vector <int> excess;
+        std::vector< std::vector <Edge> > adjList;
+        std::vector <Edge> edges;
+        Graph *levelGraph;
+
+        Graph(int n){
+            vertexCount = n;
+            adjList = std::vector < std::vector<Edge> > (n); 
+            height = std::vector < int > (n);
+            excess = std::vector < int > (n);
+        }
+        void addEdge(int u, int v, int cap){
+            Edge to{v, cap , 0, u};
+            adjList[u].push_back(to);
+        }
+        void addEdge2(int u, int v, int cap){
+            Edge to{v, cap , 0, u};
+            edges.push_back(to);
+        }
+        void preflow(int start);
+        void preflow2(int start);
+        int overFlowVertex();
+        void updateReverseEdgeFlow(int i, int j, int flow);
+        void updateReverseEdgeFlow2(int i, int flow);
+        void relabel(int start);
+        bool push2(int start);
+        int getMaxFlow(int s, int t);
+
+                        
+};
+
+void Graph::preflow(int start)
+{
+    
+    height[start] = vertexCount;
+ 
+    //put maximum flow in from source outgoing edges
+    //set flow to capacity and create reverse edge
+    //TODO: check why reverse edge necessary
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        for (int j = 0; j < adjList[i].size(); j++){
+            Edge &e = adjList[i][j];
+
+            if (e.start == start){
+                e.flow = e.capacity;
+                excess[ e.to ] += e.capacity; //+= or =  ?
+
+                Edge back{start, 0 , -e.flow, e.to};
+                adjList[start].push_back(back);
+            } 
+        }
+    }
+}
+
+
+void Graph::preflow2(int start){
+
+    height[start] = vertexCount;
+
+    for (int i = 0; i < edges.size(); i++){
+        if (edges[i].start == start){
+            edges[i].flow = edges[i].capacity;
+            excess[ edges[i].to ] += edges[i].capacity;
+
+            Edge back{start, 0 , -edges[i].flow, edges[i].to};
+            edges.push_back(back);
+        }
+    }
+}
+
+// returns index of overflowing Vertex
+int Graph::overFlowVertex()
+{
+    for (int i = 0; i < excess.size(); i++)
+       if (excess[i] > 0)
+            return i;
+ 
+    return -1;
+}
+
+// Update reverse flow for flow added on its Edge
+void Graph::updateReverseEdgeFlow(int i, int j, int flow)
+{
+    //int u = edge[i].v;
+    //int v = edge[i].u;
+    int u = adjList[i][j].to;
+    int v = adjList[i][j].start;
+ 
+    for (int n = 0; n < adjList.size(); n++)
+    {
+        for (int m = 0; m < adjList[n].size(); m++){
+            if (adjList[n][m].to == v && adjList[n][m].start == u){
+                adjList[n][m].flow -= flow;
+                return;
+            }
+        }
+    }
+
+    // adding reverse Edge in residual graph
+    Edge back{v, 0 , flow, u};
+    adjList[u].push_back(back);
+
+
+}
+
+void Graph::updateReverseEdgeFlow2(int i, int flow)
+{
+    int u = edges[i].to;
+    int v = edges[i].start;
+
+    for (int j = 0; j < edges.size(); j++)
+    {
+        if (edges[j].to == v && edges[j].start == u)
+        {
+            edges[j].flow -= flow;
+            return;
+        }
+    }
+
+    // adding reverse Edge in residual graph... why?
+    Edge back{v, 0 , flow, u};
+    edges.push_back(back);
+
+}
+
+// To push flow from overflowing vertex u
+bool Graph::push2(int start)
+{
+   
+    for (int i = 0; i < edges.size(); i++)
+    {
+        
+        if (edges[i].start == start)
+        {
+            if (edges[i].flow >= edges[i].capacity)
+                continue;
+ 
+            // Push is only possible if height of adjacent
+            // is smaller than height of overflowing vertex
+            if (height[start] > height[edges[i].to])
+            {
+                // Flow to be pushed is equal to minimum of
+                // remaining flow on edge and excess flow.
+                int flow = std::min(edges[i].capacity - edges[i].flow, excess[start]);
+ 
+                // Reduce excess flow for overflowing vertex
+                excess[start] -= flow;
+ 
+                // Increase excess flow for adjacent
+                excess[edges[i].to] += flow;
+ 
+                // Add residual flow (With capacity 0 and negative
+                // flow)
+                edges[i].flow += flow;
+ 
+                updateReverseEdgeFlow2(i, flow);
+ 
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// function to relabel vertex u
+void Graph::relabel(int start)
+{
+    // Initialize minimum height of an adjacent
+    int maxHeight = 10000000;
+ 
+    // Find the adjacent with minimum height
+    for (int i = 0; i < edges.size(); i++){
+
+        if (edges[i].start == start){
+            // if flow is equal to capacity then no
+            // relabeling
+            if (edges[i].flow == edges[i].capacity)
+                continue;
+ 
+            // Update minimum height
+            if (height[edges[i].to] < maxHeight){
+                maxHeight = height[edges[i].to];
+ 
+                // updating height of u
+                height[start] = maxHeight + 1;
+            }
+        }
+    }
+}
+// main function for printing maximum flow of graph
+int Graph::getMaxFlow(int s, int t){
+    preflow2(s);
+ 
+    // loop until none of the Vertex is in overflow
+    while (this->overFlowVertex() != -1){
+        int start = this->overFlowVertex();
+        if (!push2(start))
+            relabel(start);
+    }
+ 
+    // ver.back() returns last Vertex, whose
+    // e_flow will be final maximum flow
+    return excess[t];
+}
+
 int main (){
     
     int t;
     std::cin >> t;
     for (int i = 1; i <= t; i++) {
+        int n, m;
+        std::cin >> n >> m;
         
+        Graph graph(n);
+        for (int j = 1; j <= m; j++){
+            int u,v,cap;
+            std::cin >> u >> v >> cap;
+            //adjust indices
+            graph.addEdge2(u-1, v-1, cap);
+            graph.addEdge2(v-1, u-1, cap);
+        }
         
-        std::cout << "Case #" << i << ": " << "something" << "\n";
+
+        std::cout << "Case #" << i << ": " << graph.getMaxFlow(0,n-1) << "\n";
 
     }
     return 0;
--- week5/E.cpp
+++ week5/E.cpp
@@ -6,7 +6,7 @@
 #include <queue>
 #include <stack>
 
-
+//mostly inspired by: http://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/
 struct Edge {
     int to;
     int capacity;
@@ -29,18 +29,18 @@ class Graph {
         }
         void addEdge(int u, int v, int cap){
             Edge to{v, cap, 0 , u};
-            Edge back{u, 0, 0 , v}; //not sure what to do with this one, but should be in there
+            //Edge back{u, 0, 0 , v}; //not sure what to do with this one, but should be in there
             adjList[u].push_back(to);
-            adjList[v].push_back(back); //maybe just remove this one
+            //adjList[v].push_back(back); //maybe just remove this one
         }
-        void assignLevel(int start);
+        bool assignLevel(int start, int end);
         bool constructLevel(int start, int end);
-        int maxFlow(int current, int neighbourindex, int end, int flow);
+        int maxFlow(int current, std::vector<int> &indexCount, int end, int flow);
 };
 
 //use BFS to construct the Levelgraph
 //if the end can't be reached, it has the value of level.size()+1
-void Graph::assignLevel(int start){
+bool Graph::assignLevel(int start, int end){
     
     std::queue <int> q;
 
@@ -55,15 +55,15 @@ void Graph::assignLevel(int start){
         q.pop();
 
         for (int i = 0; i < adjList[current].size(); i++){
-            if(level[ adjList[current][i].to ] > level[current] + 1){
+            if(level[ adjList[current][i].to ] > level[current] + 1 && adjList[current][i].capacity > adjList[current][i].flow){
                 level[ adjList[current][i].to ] = level[current] + 1;
                 q.push(adjList[current][i].to);
             }
         }
     }
+    return (level[end] != level.size()+1);
 }
 
-//TODO: Change level dependency
 //returns 0 if there is no path between start and goal and 1 if there is
 //also only adds edge if it still has capacity to be of use
 bool Graph::constructLevel(int start, int goal){
@@ -102,18 +102,17 @@ bool Graph::constructLevel(int start, int goal){
 }
 
 //recursive DFS traversal for determining the flow
-int Graph::maxFlow(int current, int neighborIndex, int end, int flow){
+int Graph::maxFlow(int current, std::vector<int> &indexCount, int end, int flow){
 
-    if (current == end || neighborIndex >= levelGraph->adjList[current].size())
+    if (current == end || indexCount[current] >= adjList[current].size())
         return flow;
 
-    for (int i = 0; i < levelGraph->adjList[current].size(); i++){
+    for (int i = 0; i < adjList[current].size(); i++){
 
-        Edge &e = levelGraph->adjList[current][neighborIndex];
-        if (e.flow < e.capacity){
+        Edge &e = adjList[current][ indexCount[current] ];
+        if (level[e.to] == level[current]+1 && e.flow < e.capacity){
             int minFlow = std::min(flow, e.capacity - e.flow);
-            int recFlow = maxFlow(e.to, neighborIndex+1, end, minFlow);
-            //////This is the wrong edge! This should be the edge of the original graph!!!
+            int recFlow = maxFlow(e.to, indexCount, end, minFlow);
 
             if (recFlow > 0){
                 e.flow += recFlow;
@@ -121,10 +120,11 @@ int Graph::maxFlow(int current, int neighborIndex, int end, int flow){
             }
 
         }
+        indexCount[current]++;
     }
     
 
-    return 10;
+    return 0;
 }
 
 
@@ -146,11 +146,21 @@ int main (){
         }
         
         int j = 0;
+        int total = 0;
         int flow = 0;
-        while(graph.constructLevel(0,n-1)){
-            flow += graph.maxFlow(j,0, n-1, 10000000);
-            if (j < n-1) j++;
-            delete graph.levelGraph; //no idea if this is necessary
+        
+        while(graph.assignLevel(0,n-1)){
+            std::vector<int> indexCount (n+1);
+            flow += graph.maxFlow(j, indexCount, n-1, 10000000);
+            if (flow == 0) 
+                break;
+            total += flow;
+
+            if (j < n-1) 
+                j++;
+            else
+                break;
+            //delete graph.levelGraph; //no idea if this is necessary
         }
         std::cout << "Case #" << i << ": " << "something" << "\n";
 
